d38428d6261725904906bae80cb0aa06e686efe5
uliadib12 <uliadib@gmail.com>
2024-01-29 10:51:31 +0700

    init firebase

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-const express = require('express');
+import express from 'express';
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../key/kombet-floris-firebase-adminsdk.json");
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req: any, res: any) => {
-  res.send('Test Baru Lagi');
+  res.send('The sedulous hyena ate the antelope!');
 });
 
 app.listen(port, () => {
